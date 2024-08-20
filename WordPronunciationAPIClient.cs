b7e1facce12979595dccc9aefce8f993e6137166
APIVerve Team <hello@apiverve.com>
2024-08-20 13:36:01 -0500

    Bug Fixes

--- WordPronunciationAPIClient.cs
+++ WordPronunciationAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, wordpronunciationQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, WordPronunciationQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(wordpronunciationQueryOptions options = null)
+        public ResponseObj Execute(WordPronunciationQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(wordpronunciationQueryOptions options)
+        private string constructURL(WordPronunciationQueryOptions options)
         {
             string url = _apiEndpoint;
 
